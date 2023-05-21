d97359e7d77de7f6d87a2964730abca1782178ed
paron <aaronjude.pael@nwssu.edu.ph>
2023-05-21 17:42:51 +0800

    Commit

---
+++ app/src/main/java/com/example/hellotoast/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.hellotoast
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    private var mCount: Int = 0
+    private lateinit var mShowCount: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mShowCount = findViewById(R.id.show_count)
+    }
+
+    fun showToast(view: View) {
+        val toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT)
+        toast.show()
+    }
+
+    fun countUp(view: View) {
+        ++mCount
+        mShowCount.text = mCount.toString()
+    }
+}
